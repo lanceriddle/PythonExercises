7feb9dee8723bd6df16f7dc200323dddcbf20c56
lanceriddle <lanceriddle@gmail.com>
2022-03-09 18:29:46 -0800

    Cleaning up Wordle comments and variables.

--- wordle_py/wordle.py
+++ wordle_py/wordle.py
@@ -10,13 +10,8 @@ Wordle Archive:
     - https://www.devangthakkar.com/wordle_archive/
 Dictionary files source:
     - http://www.gwicks.net/dictionaries.htm
-"""
 
-import os
-import traceback
-
-"""
-EXAMPLES:
+EXAMPLE "TRAINING":
 matching_list = { # List of Tuples
     ("r",4),
     ("y",5)
@@ -30,29 +25,39 @@ word_not_contains_list = [ # List of chars
 ]
 """
 
-matching_list = { # List of Tuples  # TODO: Read these from an XLS??
-    ("r",2),
-    ("r",5)
+import os
+import traceback
+
+# Global Vars:
+DICT_FILE = os.path.join(os.path.dirname(__file__), 'usa.txt')
+MAX_DISPLAY_COLUMNS = 5
+column_count = 0
+
+# WORD RESTRICTIONS:
+# In the word and correct spot (Green Boxes):
+matching_list = { # List of Tuples
+    ("r",2)
 }
+# In the word but wrong spot (Yellow Boxes):
 contain_but_not_match_list = { # List of Tuples
     ("e",3),
-    ("r",4),
-    ("e",5),
-    ("o",1),
-    ("e",4)
+    ("r",4)
 }
+# Not in the word in any spot (Gray Boxes):
 word_not_contains_list = [ # List of chars
-    "q", "u", "y", "p", "i", "c", "d"
+    "q", "u"
 ]
 
 
 def word_matches_list(word, list):
+    """ Make sure the word contains the letters at the given positions (Green Boxes). """
     for letter, position in list:
         if not (letter == word[position-1].lower()):
             return False
     return True
 
 def word_contain_but_not_match_list(word, list):
+    """ Make sure the word contains the letters, but NOT at the given positions (Yellow Boxes). """
     for letter, position in list:
         if (letter == word[position-1].lower()
                 or not letter in word.lower()):
@@ -60,32 +65,31 @@ def word_contain_but_not_match_list(word, list):
     return True
 
 def word_not_contains(word, list):
+    """ Make sure the word does NOT contain any of the letters, in any position (Gray Boxes). """
     for item in list:
         if item in word.lower():
             return False
     return True
 
-max_columns = 5
-column_count = 0
-def check_word(word):
-    global column_count # Needed to modify global var
-    if (len(word) == 5):
-        if (word_matches_list(word, matching_list)
-                and word_contain_but_not_match_list(word, contain_but_not_match_list)
-                and word_not_contains(word, word_not_contains_list)
-                ):
-            if (column_count > max_columns):
-                print(word)
-                column_count = 0
-            else:
-                print(word + "     ", end = "")
-                column_count += 1
 
-input_path = os.path.join(os.path.dirname(__file__), 'usa.txt')
+def check_word(word):
+    """ For each word, run it through the restrictions and if it passes, print it to the screen. """
+    global column_count # Needed to modify global var
+    if (len(word) == 5
+            and word_matches_list(word, matching_list)
+            and word_contain_but_not_match_list(word, contain_but_not_match_list)
+            and word_not_contains(word, word_not_contains_list)):
+        column_count += 1
+        if (column_count == MAX_DISPLAY_COLUMNS):
+            print(word)
+            column_count = 0
+        else:
+            print(word + "     ", end="")
+
 
 def main():
     try:
-        with open(input_path, 'r') as file:
+        with open(DICT_FILE, 'r') as file:
             for word in file:
                 check_word(word.rstrip())
     except Exception as e:
